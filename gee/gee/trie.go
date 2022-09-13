dbff83cfef6df2c08f3d75f2ff39acddd11c93a4
Alexleslie <29724890+Alexleslie@users.noreply.github.com>
2022-09-13 15:15:34 +0800

    优化了一些关于gee的注释和代码

--- gee/gee/trie.go
+++ gee/gee/trie.go
@@ -5,18 +5,15 @@ import (
 	"strings"
 )
 
+// 路由节点
 type node struct {
-	pattern  string  //待匹配路由，例如/p/:lang，是否为一个完整URL，不是则为"”
-	part     string  //路由中的一部分，例如：lang，URL切割后的块值
-	children []*node //该节点的子节点，例如[doc,tutorial,intro]
-	isWild   bool    //是否模糊匹配，part含有；或*时候为true
+	pattern  string  // 待匹配路由，例如/p/:lang，是从根节点到当前的完整pattern，不是则为空
+	part     string  // 路由的最小组成部分，例如：lang，URL切割后的块值
+	children []*node // 该节点的子节点，例如[doc,tutorial,intro]
+	isWild   bool    // 是否模糊匹配，part含有:或*时候为true
 }
 
-func (n *node) String() string {
-	return fmt.Sprintf("node{pattern=%s, part=%s, isWild=%t}", n.pattern, n.part, n.isWild)
-}
-
-// 找第一个匹配成功的节点，用于插入（找到一个就立即返回），如果为模糊匹配也可以成功匹配上
+// 从子节点找第一个匹配成功的节点
 func (n *node) matchChild(part string) *node {
 	for _, child := range n.children {
 		if child.part == part || child.isWild {
@@ -26,7 +23,7 @@ func (n *node) matchChild(part string) *node {
 	return nil
 }
 
-// 匹配所有成功的节点后返回，用于查找，必须返回所有可能成功的子节点来进行遍历查找
+// 从子节点找所有匹配成功的节点
 func (n *node) matchChildren(part string) []*node {
 	nodes := make([]*node, 0)
 	for _, child := range n.children {
@@ -37,8 +34,9 @@ func (n *node) matchChildren(part string) []*node {
 	return nodes
 }
 
-// 先匹配后插入
-func (n *node) insert(pattern string, parts []string, height int) {
+// 从parts的height位置寻找/构造节点且连接节点
+// 直到完成parts中每个part都有对应的节点
+func (n *node) linkNode(pattern string, parts []string, height int) {
 	if len(parts) == height {
 		n.pattern = pattern
 		return
@@ -50,11 +48,11 @@ func (n *node) insert(pattern string, parts []string, height int) {
 		child = &node{part: part, isWild: part[0] == ':' || part[0] == '*'}
 		n.children = append(n.children, child)
 	}
-	child.insert(pattern, parts, height+1)
+	child.linkNode(pattern, parts, height+1)
 }
 
-// 查找匹配的URL
-func (n *node) search(parts []string, height int) *node {
+// 搜索路径的最终路由节点
+func (n *node) searchNode(parts []string, height int) *node {
 	//递归终止条件：找到末尾了或者通配符
 	if len(parts) == height || strings.HasPrefix(n.part, "*") {
 		if n.pattern == "" {
@@ -67,12 +65,15 @@ func (n *node) search(parts []string, height int) *node {
 	part := parts[height]
 	//遍历查找所有可能的子路径
 	children := n.matchChildren(part)
-
 	for _, child := range children {
-		result := child.search(parts, height+1)
+		result := child.searchNode(parts, height+1)
 		if result != nil {
 			return result
 		}
 	}
 	return nil
+}
+
+func (n *node) String() string {
+	return fmt.Sprintf("node{pattern=%s, part=%s, isWild=%t}", n.pattern, n.part, n.isWild)
 }
